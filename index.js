3bc41f4b55a57e16a8fa6880043e932a5ccd13bd
Pickky03 <vhai02092003@gmail.com>
2024-05-14 10:08:28 +0700

    Express Router

--- index.js
+++ index.js
@@ -1,52 +1,71 @@
-const http = require('http')
-const hostname = 'localhost'
-const port = 3000
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const hostname = "localhost";
+const bodyParser = require("body-parser");
+const port = 3000;
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const dishRouter = require('./Public/Route/dishRouter')
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/Public"));
 
 
-	const server = http.createServer((req, res) => {
-		console.log('Request for ' + req.url + ' by method ' + req.method);
-	
-		if (req.method == 'GET') {
-		var fileUrl;
-		if (req.url == '/') fileUrl = '/index.html';
-		else fileUrl = req.url;
-	
-		var filePath = path.resolve('./public'+fileUrl);
-		const fileExt = path.extname(filePath);
-		if (fileExt == '.html') {
-		fs.exists(filePath, (exists) => {
-		if (!exists) {
-		res.statusCode = 404;
-		res.setHeader('Content-Type', 'text/html');
-		res.end('<html><body><h1>Error 404: ' + fileUrl + 
-		' not found</h1></body></html>');
-		return;
-		}
-		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/html');
-		fs.createReadStream(filePath).pipe(res);
-		});
-		}
-		else {
-		res.statusCode = 404;
-	    res.setHeader('Content-Type', 'text/html');
-		res.end('<html><body><h1>Error 404: ' + fileUrl + 
-		' not a HTML file</h1></body></html>');
-		}
-		}
-		else {
-		res.statusCode = 404;
-		res.setHeader('Content-Type', 'text/html');
-		res.end('<html><body><h1>Error 404: ' + req.method + 
-		' not supported</h1></body></html>');
-		}
-		})
-		
-	
-	
-    	server.listen(port, hostname, () => {
-    	  console.log(`Server running at http://${hostname}:${port}`);
-    	});
-    
+app.use(bodyParser.json());
+app.use('/dishes', dishRouter)
+
+// app.all("/dishes", (req, res, next) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   next();
+// });
+
+// app.get("/dishes", (req, res, next) => {
+//   res.end("Will send all the dishes to you!");
+// });
+
+// app.post("/dishes", (req, res, next) => {
+//   res.end(
+//     "Will add the dish: " +
+//       req.body.name +
+//       " with details: " +
+//       req.body.description
+//   );
+// });
+
+// app.put("/dishes", (req, res, next) => {
+//   res.statusCode = 403;
+//   res.end("PUT operation not supported on /dishes");
+// });
+
+// app.delete("/dishes", (req, res, next) => {
+//   res.end("Deleting all dishes");
+// });
+
+// app.get("/dishes/:dishId", (req, res, next) => {
+//   res.end("Will send details of the dish: " + req.params.dishId + " to you!");
+// });
+
+// app.post("/dishes/:dishId", (req, res, next) => {
+//   res.statusCode = 403;
+//   res.end("POST operation not supported on /dishes/" + req.params.dishId);
+// });
+
+// app.put("/dishes/:dishId", (req, res, next) => {
+//   res.write("Updating the dish: " + req.params.dishId + "\n");
+//   res.end(
+//     "Will update the dish: " +
+//       req.body.name +
+//       " with details: " +
+//       req.body.description
+//   );
+// });
+
+// app.delete("/dishes/:dishId", (req, res, next) => {
+//   res.end("Deleting dish: " + req.params.dishId);
+// });
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
